702981d8cf5d8bfeec96f1bdd8904e855abbdd75
Mourad Zaher <Mourad.zaher@cicapital.com>
2026-02-17 19:36:14 +0200

    Revert main.tsx debugging to stop production boot loop

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,12 @@
-console.log('🚀 Bootstrapping main.tsx...');
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from './contexts/ThemeContext';
 
-try {
-  const rootElement = document.getElementById('root');
-  if (!rootElement) {
-    console.error('❌ Root element not found!');
-  } else {
-    console.log('📦 Mounting React app...');
-    ReactDOM.createRoot(rootElement).render(
-      <React.StrictMode>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </React.StrictMode>,
-    );
-    console.log('✅ Mount call complete.');
-  }
-} catch (err) {
-  console.error('💥 Crash in main.tsx render:', err);
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
