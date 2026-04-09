34f1db6b9b60b14073e42d15a231857da5505abe
Mourad Zaher <mourad.zaher@outlook.com>
2026-04-09 21:04:36 +0200

    verify: institutional terminal sync marker v21:05

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ThemeProvider } from './contexts/ThemeContext';
 
+console.log("INSTITUTIONAL_TERMINAL_SYNC_21_05");
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
