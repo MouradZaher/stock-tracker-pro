dce7aa751b2294cd01db8cf95a29012685d9e345
Mourad Zaher <mourad.zaher@outlook.com>
2026-04-14 23:50:59 +0200

    DEPL: Force cache purge (SYNC_21_85) and finalize Live UI sanitization

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ const queryClient = new QueryClient({
 function AppContent() {
   // GLOBAL CACHE BUSTER & VERSION TRACKER
   useEffect(() => {
-    console.log('--- INSTITUTIONAL TERMINAL SYNC_21_55 ---');
+    console.log('--- INSTITUTIONAL TERMINAL SYNC_21_85 ---');
     console.log('[Stability Engine] Verifying mobile quadrant grid...');
   }, []);
 
